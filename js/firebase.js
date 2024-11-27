53915dcaf9d05311b5c5ecdea9b832b553dc1380
conorhoner <168785011+conorhoner@users.noreply.github.com>
2024-11-27 19:23:43 +0000

    test

--- js/firebase.js
+++ js/firebase.js
@@ -2,15 +2,32 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBw3qJqCfZTwkebdTqA29trOc2f3ryhzBY",
+  authDomain: "mai-day-film.firebaseapp.com",
+  projectId: "mai-day-film",
+  storageBucket: "mai-day-film.firebasestorage.app",
+  messagingSenderId: "149266649737",
+  appId: "1:149266649737:web:a2cf2aef2bea3201d36c58",
+  measurementId: "G-5ESLTCVDYY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export { db };
+
+//test
+import { db } from './firebase.js';
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+
+async function testFirestore() {
+  try {
+    const docRef = await addDoc(collection(db, "sessions"), {
+      test: "Hello, Firestore!"
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+testFirestore();
